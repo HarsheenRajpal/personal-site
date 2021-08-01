bc1ba514a347381bf4f39e354ad0a1f66f266251
Harsheen Rajpal <62307082+KhushiRajpal@users.noreply.github.com>
2021-08-01 16:36:09 +0530

    Update site.js

--- src/data/stats/site.js
+++ src/data/stats/site.js
@@ -1,21 +1,14 @@
 /* Keys match keys returned by the github api. Fields without keys are
  * mostly jokes. To see everything returned by the github api, run:
- curl https://api.github.com/repos/mldangelo/personal-site
  */
 const data = [
   {
-    label: 'Wanna get started with JS, to build things like these : ',
-    key: 'subscribers_count',
-    link: 'https://github.com/public-apis/public-apis',
+    label: 'Number of lints',
+    value: '0 //enforced via github workflow',
   },
   {
     label: 'Number of spoons',
     value: '0',
-  },
-  {
-    label: 'Number of forks',
-    key: 'forks',
-    link: 'https://github.com/KhushiRajpal/personal-site/network',
   },
   {
     label: 'Number of spoons',
